f12500a2e5e1b4f39c4442c7a5a4c07572190a5a
戸軽健太 <n99.kenta.togaru@gmail.com>
2023-08-09 15:54:35 +0900

    修正

--- src/arrow-function.ts
+++ src/arrow-function.ts
@@ -1,11 +1,15 @@
-export { };
+export {};
 
-// let bmi: (height: number, weight: number) => number = function (
-//   height: number, weight: number): number {
-//   return weight / (height * height);
-// }
+let bmi: (height: number, weight: number) => number = function (
+  height: number, weight: number): number {
+  return weight / (height * height);
+}
 
-let bmi: (height: number, weight: number) => number = (
+let bmiArrow: (height: number, weight: number) => number = (
   height: number,
   weight: number
 ): number => weight / (height * height);
+
+console.log(bmi(1.60, 56));
+console.log(bmiArrow(1.72, 60));
+
