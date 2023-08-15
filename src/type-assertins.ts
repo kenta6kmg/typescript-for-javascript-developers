bde5e73e1bd271b4b24e0a20e7e7a14e8dd11b1b
戸軽健太 <n99.kenta.togaru@gmail.com>
2023-08-15 16:18:06 +0900

    型アサーション

---
+++ src/type-assertins.ts
@@ -0,0 +1,9 @@
+export {};
+
+let name: any = 'wood';
+
+// let length = name.length as number;
+// let length = (name as string).length;
+let length = (<string>name).length;
+
+// length = 'stick';
