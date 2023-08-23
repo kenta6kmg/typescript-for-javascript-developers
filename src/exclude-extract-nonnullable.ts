a1a3dd25d938e915f71efa8c73714dfde0cc5b63
戸軽健太 <n99.kenta.togaru@gmail.com>
2023-08-23 13:23:31 +0900

    Exclude, Extract, Nonnullable

---
+++ src/exclude-extract-nonnullable.ts
@@ -0,0 +1,22 @@
+export {};
+
+type DebugType = () => void;
+
+type SomeTypes = string | number | DebugType;
+
+// ある型だけ除外したい。
+// Excludeを使用する。<全体のユニオン型、除外したい型>
+type FunctionType = Exclude<SomeTypes, string | number>;
+type NunFunctionType = Exclude<SomeTypes, DebugType>;
+// 関数型を全て除外
+type TypeExcludingFunction = Exclude<SomeTypes, Function>;
+
+// Extract(抽出する)
+type FunctionTypeByExtract = Extract<SomeTypes, DebugType>;
+type NonFunctionByExtract = Extract<SomeTypes, string | number>;
+type FunctionTypeExtractingFunction = Extract<SomeTypes, Function>;
+
+// Nonnullable
+// ユニオン型からnull,undefined
+type NullableTypes = string | number | null | undefined;
+type NonnullableTypes = NonNullable<NullableTypes>
